048e0a3858b3101d3a5487db37594623835b5aa8
Lucas Badiale <lcbadial@gmail.com>
2011-06-29 02:14:54 -0300

    commit default

--- src/org/xinvest/beans/Bank.java
+++ src/org/xinvest/beans/Bank.java
@@ -29,32 +29,49 @@ public class Bank extends User {
     public void setInterest(Float interest) { this.interest = interest; }
     public Float getInterest() { return this.interest; }
 	
-	public static Bank find (Integer id) {
+	public static Bank find (String email) {
 		Session session = DBManager.getSession();
-		return (Bank) session.load(Bank.class, id);
+		return (Bank) session.load(Bank.class, email);
 	}
     
     private static void Test01 () {
+		Session session = DBManager.getSession();
+		session.beginTransaction();
         Bank bank = new Bank();
-        bank.setEmail("bank");
+        bank.setEmail("bank2");
         bank.setName("Bank");
         bank.setPassword("bank");
         bank.setMoney(new Float(100000));
         bank.setInterest(new Float(0.5));
+		for (int i = 0; i < 10; i++) {
+			Transaction t = new Transaction();
+			t.setPassive(bank);
+			t.setValue(new Float(1000 * (i + 1)));
+			t.insert();
+			bank.getTransactionActives().add(t);
+		}
         try {
             bank.insert();
         } catch (Exception e) {
             System.err.println("ERRO:Test01:INSERT: "+e);
             e.printStackTrace();
         }
-        Bank bankdb = (Bank) User.find("bank");
+        Bank bankdb = Bank.find("bank2");
         System.out.println("Email: "+bankdb.getEmail());
         System.out.println("Name: "+bankdb.getName());
         System.out.println("Pass: "+bankdb.getPassword());
         System.out.println("Money: "+bankdb.getMoney());
         System.out.println("Interest: "+bankdb.getInterest());
+		Iterator it2 = bankdb.getTransactionActives().iterator();
+		while (it2.hasNext()) {
+			Transaction t = (Transaction) it2.next();
+			System.out.println("\tData da transacao: " + t.getTimestamp());
+			System.out.println("\tValor da transacao: " + t.getValue());
+		}
+		System.out.println("");
         bankdb.setInterest(new Float(0.8));
         bankdb.update();
+		session.getTransaction().commit();
     }
     
     public static void main (String args[]) {
